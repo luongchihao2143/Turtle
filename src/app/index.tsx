b00d8efe085c7f4cd617a2c4b738412aad63d6a9
luongchihao2143 <luongchihao2143@gmail.com>
2024-07-16 21:14:14 +0700

    Move src

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text>hello</Text>
+    </View>
+  );
+}
